b610b311744650a3ae2c06b09c7f4326202408e6
Aaron Cruz <pferdefleisch+git@gmail.com>
2023-06-26 22:08:54 +0200

    Better error handling

--- lib/api/real.ex
+++ lib/api/real.ex
@@ -1,5 +1,5 @@
 defmodule Amplitude.API.Real do
-  @headers [content_type: "application/json"]
+  @headers [content_type: "application/json", accept: "application/json"]
 
   defp api_host, do: Application.get_env(:amplitude, :api_host)
   defp api_key, do: Application.get_env(:amplitude, :api_key)
@@ -12,8 +12,13 @@ defmodule Amplitude.API.Real do
       {:ok, %Req.Response{status: 200, body: body}} ->
         {:ok, body}
 
-      {:ok, %Req.Response{status: 404, body: body}} ->
-        {:error, body}
+      {:ok, %Req.Response{status: 400, body: body}} ->
+        IO.inspect(body, label: "body", pretty: true, limit: :infinity, printable_limit: :infinity)
+
+        {:error, "Error from Amplitude: #{inspect(body)}"}
+
+      {:ok, %Req.Response{status: status, body: body}} ->
+        {:error, "Request with status: #{status} => #{inspect(body)}"}
 
       {:error, exception} ->
         {:error, exception}
